cd6e957501edc3e99136f2d6a763b5e87a1bc8e5
subaru365 <subaru.woody@gmail.com>
2016-02-24 17:29:47 +0900

    create: List 2-6, 2-8
    	new file:   SumAve1.java
    	new file:   SumAve3.java

---
+++ chap02/SumAve1.java
@@ -0,0 +1,15 @@
+class SumAve1 {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("Sum : " + (x + y));
+		System.out.println("Average : " + (x + y) / 2);
+	}
+}
---
+++ chap02/SumAve3.java
@@ -0,0 +1,12 @@
+class SumAve3 {
+
+	public static void main(String[] args) {
+		int x = 63;
+		int y = 18;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("Sum : " + (x + y));
+		System.out.println("Average : " + (x + y) / 2);
+	}
+}
