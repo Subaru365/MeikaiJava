95771392a0a1af335251cdaa042f968b6b9a8301
subaru365 <subaru.woody@gmail.com>
2016-02-24 16:55:34 +0900

    create: List 1-3, 1-4
    	new file:   HowAreYou2.java
    	new file:   HowAreYou3.java

---
+++ chap01/HowAreYou2.java
@@ -0,0 +1,6 @@
+class HowAreYou2 {
+
+	public static void main(String[] args) {
+		System.out.print("こんにちは！" + "元気ですか？");
+	}
+}
---
+++ chap01/HowAreYou3.java
@@ -0,0 +1,6 @@
+class HowAreYou3 {
+
+	public static void main(String[] args) {
+		System.out.print("こんにちは！\n元気ですか？");
+	}
+}
