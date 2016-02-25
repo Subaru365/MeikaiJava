2054fc2e25911a5583eb454d033dde4d2835fe2d
subaru365 <subaru.woody@gmail.com>
2016-02-25 20:02:55 +0900

    create: List 5-1
    	new file:   DecOctHexLiteral.java

---
+++ chap05/DecOctHexLiteral.java
@@ -0,0 +1,12 @@
+class DecOctHexLiteral {
+
+	public static void main(String[] args) {
+		int a = 13;
+		int b = 013;
+		int c = 0x13;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
+}
