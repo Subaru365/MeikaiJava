62e7bdc70f680757667a84fa7a9233f6c1940040
subaru365 <subaru.woody@gmail.com>
2016-02-26 16:27:48 +0900

    create; List 6-3
    	new file:   IntArray.java

---
+++ chap06/IntArray.java
@@ -0,0 +1,14 @@
+class IntArray {
+
+	public static void main(String[] args) {
+		int[] a = new int[5];
+
+		for (int i = 0; i < a.length; i++) {
+			a[i] = i + 1;
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.printf("a[%d] = %d\n", i, a[i]);
+		}
+	}
+}
