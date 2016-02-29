cbba6787edf96b3c9e9ffda2f1ef38666b3115f1
subaru365 <subaru.woody@gmail.com>
2016-02-29 17:03:03 +0900

    create: List 9-3
    	new file:   DayAssign.java

---
+++ chap09/day1/DayAssign.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class DayAssign {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		String[] wd = {"日","月","火","水","木","金","土"};
+
+		System.out.println("誕生日を西暦で入力せよ。");
+		System.out.print("年 ： ");	int y = stdIn.nextInt();
+		System.out.print("月 ： ");	int m = stdIn.nextInt();
+		System.out.print("日 ： ");	int d = stdIn.nextInt();
+
+		Day birthday = new Day(y, m, d);
+
+		System.out.println("あなたの誕生日"
+			+ birthday.getYear()	+ "年"
+			+ birthday.getMonth()	+ "月"
+			+ birthday.getDate()	+ "日"
+			+ wd[birthday.dayOfWeek()] + "曜日です。"
+		);
+
+		Day xDay = birthday;
+		xDay.set(2100, 12, 31);
+
+
+		System.out.println("birthday = "
+			+ birthday.getYear()	+ "年"
+			+ birthday.getMonth()	+ "月"
+			+ birthday.getDate()	+ "日"
+			+ wd[birthday.dayOfWeek()] + "曜日です。"
+		);
+
+
+		System.out.println("xDay = "
+			+ xDay.getYear()	+ "年"
+			+ xDay.getMonth()	+ "月"
+			+ xDay.getDate()	+ "日"
+			+ wd[xDay.dayOfWeek()] + "曜日です。"
+		);
+
+	}
+}
