8f08fb95f65b748641e69c4695194a4a31b0a455
subaru365 <subaru.woody@gmail.com>
2016-03-05 22:20:15 +0900

    create: List 13-3 ~ 13-8
    	new file:   AbstLine.java
    	new file:   HorzLine.java
    	new file:   Point.java
    	new file:   Rectangle.java
    	new file:   Shape.java
    	new file:   VirtLine.java

---
+++ chap13/shape2/HorzLine.java
@@ -0,0 +1,18 @@
+
+public class HorzLine extends AbstLine {
+	
+	public HorzLine(int length) {
+		super(length);
+	}
+	
+	public String toString() {
+		return "HorzLine(length:" + getLength() + ")";
+	}
+	
+	public void draw() {
+		for (int i = 1; i <= getLength(); i++) {
+			System.out.print('-');
+		}
+		System.out.println();
+	}
+}
---
+++ chap13/shape2/Rectangle.java
@@ -0,0 +1,25 @@
+
+public class Rectangle extends Shape {
+	
+	private int width;
+	private int height;
+	
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle(width:" + width + ", height" + height + ")";
+	}
+	
+	public void draw() {
+		for (int i = 1; i <= height; i++) {
+			for (int j = 1; j <= width; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
