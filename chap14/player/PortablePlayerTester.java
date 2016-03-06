f37bb0f53df37faee10f1b2545940c6dcf980bab
subaru365 <subaru.woody@gmail.com>
2016-03-06 16:41:26 +0900

    create: List 14-10
    	new file:   PortablePlayerTester.java

---
+++ chap14/player/PortablePlayerTester.java
@@ -0,0 +1,10 @@
+
+public class PortablePlayerTester {
+
+	public static void main(String[] args) {
+		PortablePlayer a = new PortablePlayer();
+		a.play();
+		a.stop();
+		a.changeSkin(Skinnable.LEOPARD);
+	}
+}
